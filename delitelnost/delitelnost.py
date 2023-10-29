0b6fe87d642b48d12b0bb15b32198de8a8abe820
Radovan Holčík <78747191+KermitThePog@users.noreply.github.com>
2023-10-29 14:18:48 +0100

    Update delitelnost.py

--- delitelnost/delitelnost.py
+++ delitelnost/delitelnost.py
@@ -1,3 +1,7 @@
 #by Radovan Holčík (KermitThePog)
 n = int(input("Zadej libovolné přirozené číslo: "))
 for i in range(1, n+1): print((lambda j : str(i)+" " if(n%j==0) else "")(i), end="")
+
+#print( (lambda n: str(n)) (int(input("Zadej libovolné přirozené číslo: "))) )
+
+#print([x for x in range(n = int(input("Zadej libovolné přirozené číslo: "))) if n%x==0])
